fc77a9c404db222373b0b7155bb44106066afb2f
Victor Stepanov <victor.a.stepanov@gmail.com>
2022-10-09 21:26:45 -0700

    adding code for the "working with color palettes in a json file" tutorial

---
+++ working_with_color_palettes/color_palettes_done.py
@@ -0,0 +1,364 @@
+import json
+import math
+import os
+import random
+import time
+
+import bpy
+
+################################################################
+# helper functions BEGIN
+################################################################
+
+
+def purge_orphans():
+    """
+    Remove all orphan data blocks
+
+    see this from more info:
+    https://youtu.be/3rNqVPtbhzc?t=149
+    """
+    if bpy.app.version >= (3, 0, 0):
+        # run this only for Blender versions 3.0 and higher
+        bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
+    else:
+        # run this only for Blender versions lower than 3.0
+        # call purge_orphans() recursively until there are no more orphan data blocks to purge
+        result = bpy.ops.outliner.orphans_purge()
+        if result.pop() != "CANCELLED":
+            purge_orphans()
+
+
+def clean_scene():
+    """
+    Removing all of the objects, collection, materials, particles,
+    textures, images, curves, meshes, actions, nodes, and worlds from the scene
+
+    Checkout this video explanation with example
+
+    "How to clean the scene with Python in Blender (with examples)"
+    https://youtu.be/3rNqVPtbhzc
+    """
+    # make sure the active object is not in Edit Mode
+    if bpy.context.active_object and bpy.context.active_object.mode == "EDIT":
+        bpy.ops.object.editmode_toggle()
+
+    # make sure non of the objects are hidden from the viewport, selection, or disabled
+    for obj in bpy.data.objects:
+        obj.hide_set(False)
+        obj.hide_select = False
+        obj.hide_viewport = False
+
+    # select all the object and delete them (just like pressing A + X + D in the viewport)
+    bpy.ops.object.select_all(action="SELECT")
+    bpy.ops.object.delete()
+
+    # find all the collections and remove them
+    collection_names = [col.name for col in bpy.data.collections]
+    for name in collection_names:
+        bpy.data.collections.remove(bpy.data.collections[name])
+
+    # in the case when you modify the world shader
+    # delete and recreate the world object
+    world_names = [world.name for world in bpy.data.worlds]
+    for name in world_names:
+        bpy.data.worlds.remove(bpy.data.worlds[name])
+    # create a new world data block
+    bpy.ops.world.new()
+    bpy.context.scene.world = bpy.data.worlds["World"]
+
+    purge_orphans()
+
+
+def active_object():
+    """
+    returns the active object
+    """
+    return bpy.context.active_object
+
+
+def time_seed():
+    """
+    Sets the random seed based on the time
+    and copies the seed into the clipboard
+    """
+    seed = time.time()
+    print(f"seed: {seed}")
+    random.seed(seed)
+
+    # add the seed value to your clipboard
+    bpy.context.window_manager.clipboard = str(seed)
+
+    return seed
+
+
+def add_ctrl_empty(name=None):
+
+    bpy.ops.object.empty_add(type="PLAIN_AXES", align="WORLD")
+    empty_ctrl = active_object()
+
+    if name:
+        empty_ctrl.name = name
+    else:
+        empty_ctrl.name = "empty.cntrl"
+
+    return empty_ctrl
+
+
+def make_active(obj):
+    bpy.ops.object.select_all(action="DESELECT")
+    obj.select_set(True)
+    bpy.context.view_layer.objects.active = obj
+
+
+def track_empty(obj):
+    """
+    create an empty and add a 'Track To' constraint
+    """
+    empty = add_ctrl_empty(name=f"empty.tracker-target.{obj.name}")
+
+    make_active(obj)
+    bpy.ops.object.constraint_add(type="TRACK_TO")
+    bpy.context.object.constraints["Track To"].target = empty
+
+    return empty
+
+
+def setup_camera(loc, rot):
+    """
+    create and setup the camera
+    """
+    bpy.ops.object.camera_add(location=loc, rotation=rot)
+    camera = active_object()
+
+    # set the camera as the "active camera" in the scene
+    bpy.context.scene.camera = camera
+
+    # set the Focal Length of the camera
+    camera.data.lens = 70
+
+    camera.data.passepartout_alpha = 0.9
+
+    empty = track_empty(camera)
+
+    return empty
+
+
+def set_1080px_square_render_res():
+    """
+    Set the resolution of the rendered image to 1080 by 1080
+    """
+    bpy.context.scene.render.resolution_x = 1080
+    bpy.context.scene.render.resolution_y = 1080
+
+
+def set_scene_props(fps, loop_seconds):
+    """
+    Set scene properties
+    """
+    frame_count = fps * loop_seconds
+
+    scene = bpy.context.scene
+    scene.frame_end = frame_count
+
+    # set the world background to black
+    world = bpy.data.worlds["World"]
+    if "Background" in world.node_tree.nodes:
+        world.node_tree.nodes["Background"].inputs[0].default_value = (0, 0, 0, 1)
+
+    scene.render.fps = fps
+
+    scene.frame_current = 1
+    scene.frame_start = 1
+
+    scene.eevee.use_bloom = True
+    scene.eevee.bloom_intensity = 0.005
+
+    # set Ambient Occlusion properties
+    scene.eevee.use_gtao = True
+    scene.eevee.gtao_distance = 4
+    scene.eevee.gtao_factor = 5
+
+    scene.eevee.taa_render_samples = 64
+
+    scene.view_settings.look = "Very High Contrast"
+
+    set_1080px_square_render_res()
+
+
+def setup_scene(i=0):
+    fps = 30
+    loop_seconds = 12
+    frame_count = fps * loop_seconds
+
+    project_name = "color_palettes"
+    bpy.context.scene.render.image_settings.file_format = "FFMPEG"
+    bpy.context.scene.render.ffmpeg.format = "MPEG4"
+    bpy.context.scene.render.filepath = f"/tmp/project_{project_name}/loop_{i}.mp4"
+
+    seed = 0
+    if seed:
+        random.seed(seed)
+    else:
+        time_seed()
+
+    # Utility Building Blocks
+    clean_scene()
+    set_scene_props(fps, loop_seconds)
+
+    loc = (0, 0, 15)
+    rot = (0, 0, 0)
+    setup_camera(loc, rot)
+
+    context = {
+        "frame_count": frame_count,
+    }
+
+    return context
+
+
+def hex_color_str_to_rgba(hex_color: str):
+    """
+    Converting from a color in the form of a hex triplet string (en.wikipedia.org/wiki/Web_colors#Hex_triplet)
+    to a Linear RGB with an Alpha of 1.0
+
+    Supports: "#RRGGBB" or "RRGGBB"
+    """
+    # remove the leading '#' symbol if present
+    if hex_color.startswith("#"):
+        hex_color = hex_color[1:]
+
+    assert len(hex_color) == 6, "RRGGBB is the supported hex color format"
+
+    # extracting the Red color component - RRxxxx
+    red = int(hex_color[:2], 16)
+    # dividing by 255 to get a number between 0.0 and 1.0
+    srgb_red = red / 255
+    linear_red = convert_srgb_to_linear_rgb(srgb_red)
+
+    # extracting the Green color component - xxGGxx
+    green = int(hex_color[2:4], 16)
+    # dividing by 255 to get a number between 0.0 and 1.0
+    srgb_green = green / 255
+    linear_green = convert_srgb_to_linear_rgb(srgb_green)
+
+    # extracting the Blue color component - xxxxBB
+    blue = int(hex_color[4:6], 16)
+    # dividing by 255 to get a number between 0.0 and 1.0
+    srgb_blue = blue / 255
+    linear_blue = convert_srgb_to_linear_rgb(srgb_blue)
+
+    alpha = 1.0
+    return tuple([linear_red, linear_green, linear_blue, alpha])
+
+
+def convert_srgb_to_linear_rgb(srgb_color_component):
+    """
+    Converting from sRGB to Linear RGB
+    based on https://en.wikipedia.org/wiki/SRGB#From_sRGB_to_CIE_XYZ
+    """
+    if srgb_color_component <= 0.04045:
+        linear_color_component = srgb_color_component / 12.92
+    else:
+        linear_color_component = math.pow((srgb_color_component + 0.055) / 1.055, 2.4)
+
+    return linear_color_component
+
+
+################################################################
+# helper functions END
+################################################################
+
+
+def load_color_palettes():
+    """
+    download the json file with the palettes from
+    https://github.com/CGArtPython/get-color-palettes-py/blob/main/palettes/100_five_color_palettes.json
+    """
+
+    # create path to json file with color palettes
+    # on Windows this path should resolve to C:/tmp/100_five_color_palettes.json
+    path = os.path.abspath("/tmp/100_five_color_palettes.json")
+    if os.name != 'nt':
+        # when running on macOS or Linux
+        # this path should resolve to ~/tmp/100_five_color_palettes.json
+        user_folder = os.path.expanduser("~")
+        path = user_folder + path
+
+    # load the color palettes
+    with open(path, "r") as color_palettes_file:
+        color_palettes = json.loads(color_palettes_file.read())
+
+    return color_palettes
+
+
+def select_random_color_palette(context):
+    return random.choice(context["color_palettes"])
+
+
+def create_color_plane(hex_color):
+    # add a plane into the scene and scale it down
+    bpy.ops.mesh.primitive_plane_add()
+    plane = active_object()
+    plane.scale.y *= 0.2
+
+    # create a material and apply the provided hex color to the base color
+    material = bpy.data.materials.new(name=f"hex_color_{hex_color}")
+    material.diffuse_color = hex_color_str_to_rgba(hex_color)
+
+    # add material to object
+    plane.data.materials.append(material)
+
+    return plane
+
+
+def create_text_object(text):
+    bpy.ops.object.text_add()
+
+    text_obj = active_object()
+    text_obj.scale *= 0.3
+    text_obj.data.body = text
+
+    bpy.ops.object.origin_set(type="ORIGIN_CENTER_OF_MASS", center="MEDIAN")
+    text_obj.location = 0, 0, 0
+    text_obj.data.extrude = 0.07
+    text_obj.data.fill_mode = "NONE"
+    text_obj.data.bevel_depth = 0.02
+
+    return text_obj
+
+
+def parent_to_empty(text_obj, plane_obj, name):
+    empty_obj = add_ctrl_empty(name)
+    text_obj.parent = empty_obj
+    plane_obj.parent = empty_obj
+    return empty_obj
+    
+
+def create_centerpiece(context):
+
+    color_palette = select_random_color_palette(context)
+
+    current_y = 0
+    for hex_color in color_palette:
+        text_obj = create_text_object(hex_color)
+        plane_obj = create_color_plane(hex_color)
+
+        empty_obj = parent_to_empty(text_obj, plane_obj, hex_color)
+        empty_obj.location.y = current_y
+
+        current_y += 0.4
+
+
+def main():
+    """
+    Python code to generate a scene with 5 planes
+    and apply a random 5 color palette to them
+    """
+    context = setup_scene()
+    context["color_palettes"] = load_color_palettes()
+    create_centerpiece(context)
+
+
+if __name__ == "__main__":
+    main()
